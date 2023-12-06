ee7f43a48a8f7c7595a4b25a61ae755e4b8db8b3
avisinghss <anubhavsinghrr@gmail.com>
2023-12-06 14:53:39 +0530

    increasing count of cart added and css handeled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,83 @@
+import { Component } from "react";
 import MovieList from "./MovieList";
-import NavBar from "./Navbar";
-function App() {
-  return (
-    <>
-      <NavBar />
-      <MovieList />
-    </>
-  );
+import Navbar from "./Navbar.js";
+import { movies } from "./AnimeData";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: movies,
+      cartCount: 0,
+    };
+  }
+  handleAddStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars < 5) {
+      movies[movieId].stars += 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleDecStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars > 0) {
+      movies[movieId].stars -= 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].fav = !movies[movieId].fav;
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleAddtocart = (movie) => {
+    let { movies, cartCount } = this.state;
+    const movieId = movies.indexOf(movie);
+    movies[movieId].cart = !movies[movieId].cart;
+    if (movies[movieId].cart) {
+      cartCount += 1;
+    } else {
+      cartCount -= 1;
+    }
+    this.setState({
+      movies,
+      cartCount,
+    });
+  };
+  render() {
+    const { movies, cartCount } = this.state;
+    return (
+      <>
+        <Navbar cartCount={cartCount} />
+        <MovieList
+          movies={movies}
+          key={movies.id}
+          onIncStars={this.handleAddStars}
+          onDecStars={this.handleDecStars}
+          onClickFav={this.handleToggleFav}
+          onClickAddtocart={this.handleAddtocart}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,23 +1,26 @@
-import React, { Component } from "react";
-import cartpath from "./cart-loop.gif"
+import styles from "./nav.module.css";
+import cartpath from "./cart-loop.gif";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <div className="nav-bar">
-        <div className="left">
-          <h1>AnimeKart</h1>
+function NavbarOne(props) {
+  const { cartCount } = props;
+  return (
+    <>
+      <div className={styles.nav}>
+        <div className={styles.logo}>
+          <div className={styles.title}>AnimeKart</div>
           <img
+            className={styles.img}
             alt="logo"
             src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/152fff4c-8eaf-4748-8afb-82f3feb1fc23/ddsj4qm-48b77969-ff6b-4800-814b-611717750cf9.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE1MmZmZjRjLThlYWYtNDc0OC04YWZiLTgyZjNmZWIxZmMyM1wvZGRzajRxbS00OGI3Nzk2OS1mZjZiLTQ4MDAtODE0Yi02MTE3MTc3NTBjZjkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Opa3gJLTMZK_ogzTlguFK_qZjNTBxKRXKrXNEGuGbE8"
           />
         </div>
-        <div className="right">
-          <img alt="cart" src={cartpath} />
+        <div className={styles.cartIconContainer}>
+          <img className={styles.cartIcon} alt="Cart-Icon" src={cartpath} />
+          <span className={styles.cartCount}>{cartCount}</span>
         </div>
       </div>
-    );
-  }
+    </>
+  );
 }
 
-export default NavBar;
+export default NavbarOne;
